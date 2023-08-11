c7f82266007e8a6504a5b998751111444ea57085
Mohammed Helal <mohd.omar.helal@gmail.com>
2023-08-11 17:35:49 +0800

    Updating Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import Card from "./UI/Card.jsx";
+
 function App() {
   return (
     <>
-      <div id="card" style={{}}>
-        <img src="../images/image-qr-code.png" />
-        <div id="paragraphs">
-          <p id="first">Improve your front-end skills by building projects</p>
-          <p id="second">
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level.
-          </p>
-        </div>
+      <div id="container">
+        <Card />
+        <Card />
       </div>
     </>
   );
---
+++ src/UI/Card.jsx
@@ -0,0 +1,18 @@
+//import React from 'react'
+
+function Card() {
+  return (
+    <div id="card">
+      <img src="../images/image-qr-code.png" />
+      <div id="paragraphs">
+        <p id="first">Improve your front-end skills by building projects</p>
+        <p id="second">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
