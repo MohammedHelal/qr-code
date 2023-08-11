be7b22d3fd2aea85ac56e18602de2ce0f5891ac1
Mohammed Helal <mohd.omar.helal@gmail.com>
2023-08-11 18:49:31 +0800

    something

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/qr-code",
-=======
   base: "/qr-code/",
->>>>>>> 1ea4aaf57e9b6e7bee6173b477646f0ff59c0484
   plugins: [react()],
 });
